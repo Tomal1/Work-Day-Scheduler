845b2a9d027ad755a5dfcdd95737870d352f0824
Minhazul Islam <minhazulislam@hotmail.com>
2023-02-17 13:38:37 +0000

    all different variables

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,14 @@
 var currentTimeDisplay = $("#currentDay");
 currentTimeDisplay.text(moment().format('llll'));
 
+var hourNineText = $("#hour-9");
+var hourTenText = $("#hour-10");
+var hourElevenText = $("#hour-11");
+var hourTwelveText = $("#hour-12");
+var hourThirteenText = $("#hour-13");
+var hourFourteenText = $("#hour-14");
+var hourFifteenText = $("#hour-15");
+var hourSixteenText = $("#hour-16");
+var hourSeventeenText = $("#hour-17");
+var confirmationMsg = $("#confirmation-msg");
+var timeBlock = $(".time-block")
